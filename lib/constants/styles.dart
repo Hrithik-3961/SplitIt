6cf8bd046fdc38eea871c463b80698fb601ec6a4
Hrithik Agarwal <agarwal.hrithik3961@gmail.com>
2025-07-13 17:01:12 +0530

    Code Refactored

--- lib/constants/styles.dart
+++ lib/constants/styles.dart
@@ -1,24 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
-import 'package:splitit/constants/strings.dart';
 
 import 'package:splitit/constants/values.dart';
+import 'package:splitit/utils/base_util.dart';
 
 class Styles {
+  static get defaultTextStyle =>
+      const TextStyle(fontSize: Values.defaultTextSize);
 
-  static get defaultTextStyle => const TextStyle(fontSize: Values.defaultTextSize);
-
-  static get expenseTextStyle => const TextStyle(fontSize: Values.largeTextSize);
+  static get expenseTextStyle =>
+      const TextStyle(fontSize: Values.largeTextSize);
 
   static get expenseInputDecoration => InputDecoration(
-      hintText: toCurrencyString(
-        "",
-        leadingSymbol: Strings.rupeeSign,
-        useSymbolPadding: true,
-      ),
-      border: InputBorder.none);
+      hintText: BaseUtil.getFormattedCurrency(""), border: InputBorder.none);
 
-  static InputDecoration getFormDecoration(String hint) => InputDecoration(
-      hintText: hint, filled: true, border: InputBorder.none
-  );
+  static InputDecoration getFormDecoration(String hint) =>
+      InputDecoration(hintText: hint, filled: true, border: InputBorder.none);
 }
--- lib/utils/base_util.dart
+++ lib/utils/base_util.dart
@@ -1,7 +1,16 @@
 import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
+import 'package:splitit/constants/strings.dart';
 
 class BaseUtil {
   static double? getNumericValue(String currencyString) {
     return double.tryParse(toNumericString(currencyString, allowPeriod: true));
+  }
+  
+  static String getFormattedCurrency(String value) {
+    return toCurrencyString(
+      value,
+      leadingSymbol: Strings.rupeeSign,
+      useSymbolPadding: true,
+    );
   }
 }
